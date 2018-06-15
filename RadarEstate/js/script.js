dfda852bb8c41a469285c5a8d2f6d90d349a3c53
Ivan Stolyarov <ivan.stolyarov.ua@gmailcom>
2018-06-16 02:30:23 +0300

    t

--- RadarEstate/js/script.js
+++ RadarEstate/js/script.js
@@ -48,21 +48,21 @@ if (showTextBtn) {
 }
 
 // let allLinks = document.querySelectorAll("a");
-// for (let i = 0; i < allLinks.length; i++){
-//     allLinks[i].addEventListener("click", function () {
-//         let element = this;
-//         if(element.getAttribute("target")) {
-//             return;
-//         }
-//         event.preventDefault();
-//         let link = element.getAttribute("href");
-//         element.classList.add("active-link");
-//         setTimeout(function(){
-//             location.href = link;
-//             element.classList.remove("active-link");
-//         },400);
-//     });
-// }
+for (let i = 0; i < allLinks.length; i++){
+    allLinks[i].addEventListener("click", function () {
+        let element = this;
+        if(element.getAttribute("target")) {
+            return;
+        }
+        event.preventDefault();
+        let link = element.getAttribute("href");
+        element.classList.add("active-link");
+        setTimeout(function(){
+            location.href = link;
+            element.classList.remove("active-link");
+        },400);
+    });
+}
 
 // const estateItems = document.querySelectorAll(".zim-real-estate__item");
 // if(estateItems) {
