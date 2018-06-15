39a35ede633ddb6e0cfbe4f90fa8f3a4d7d24d48
Ivan Stolyarov <ivan.stolyarov.ua@gmailcom>
2018-06-16 02:11:03 +0300

    t

--- RadarEstate/js/script.js
+++ RadarEstate/js/script.js
@@ -114,19 +114,19 @@ mobileMenuBtnShow.addEventListener("click", function () {
 // }
 
 // Form validation
-let form = document.querySelector("#form");
-if(form) {
-    let emailInput = form.email;
-    let emptyValue = emailInput.value;
-    emailInput.addEventListener("input", function (event) {
-        let validMail = event.target.value.match(/^[0-9a-z-\.-\@]+$/i);
-        if((emptyValue == event.target.value) || (validMail)) {
-            document.querySelector(".form__email").classList.remove("form__email-wrong");
-        } else {
-            document.querySelector(".form__email").classList.add("form__email-wrong");
-        }
-    });
-}
+// let form = document.querySelector("#form");
+// if(form) {
+//     let emailInput = form.email;
+//     let emptyValue = emailInput.value;
+//     emailInput.addEventListener("input", function (event) {
+//         let validMail = event.target.value.match(/^[0-9a-z-\.-\@]+$/i);
+//         if((emptyValue == event.target.value) || (validMail)) {
+//             document.querySelector(".form__email").classList.remove("form__email-wrong");
+//         } else {
+//             document.querySelector(".form__email").classList.add("form__email-wrong");
+//         }
+//     });
+// }
 
 
 
