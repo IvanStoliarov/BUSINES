b77aed6184211b87ede152f1ffa46925e0d2f60b
Ivan Stolyarov <ivan.stolyarov.ua@gmailcom>
2018-06-21 16:46:31 +0300

    2106

---
+++ RadarEstate/js/complex_carousel.js
@@ -0,0 +1,14 @@
+var elem = document.querySelector('.apartment-carousel');
+var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    freeScroll: true,
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.apartment-carousel', {
+    // options
+});
